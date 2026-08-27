1c8a411d37429546c52956c18605e450804d2f45
gourabd1ms <gourabd@morganstanley.com>
2026-08-27 17:53:14 -0400

    Add src/test/kotlin/MainTest.kt

---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,9 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MainTest {
+    @Test
+    fun greetReturnsExpectedMessage() {
+        assertEquals("Hello from Kotlin demo!", greet())
+    }
+}
