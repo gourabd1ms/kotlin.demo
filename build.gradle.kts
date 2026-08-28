cf6ff5e2fdffe464d403571fa04a94da52f5d6fa
gourabd1ms <gourabd@morganstanley.com>
2026-08-28 14:04:18 -0400

    Add coverage tooling and rewrite CI workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.22"
     application
+    jacoco
 }
 
 repositories {
@@ -17,4 +18,12 @@ application {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+    }
 }
